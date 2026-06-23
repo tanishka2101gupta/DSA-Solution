d5971d3bcd3a1124554075d56ee57ac804d783dd
Tanishka gupta <tanishkag00@gmail.com>
2026-06-23 12:48:18 +0530

    Time: 8 ms (4.65%), Space: 71.8 MB (51.51%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    int maxSubArray(vector<int>& nums)
+    {
+        int currsum=nums[0];
+        int maxsum=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            currsum=max(nums[i],currsum+nums[i]);
+            maxsum=max(maxsum,currsum);
+        }
+        return maxsum;
+        
+    }
+};
